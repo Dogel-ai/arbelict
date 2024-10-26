b543f0b7176aeff5e9d72c1f52cf07a6dfd74a18
Dogel-ai <dogel.kszb@proton.me>
2024-10-26 02:19:14 +0100

    feat: add unzipper function

--- internal/installer/install.go
+++ internal/installer/install.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"bufio"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -22,7 +24,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Installation failed: ", err)
 	}
-	log.Print("Binaries downloaded successfully")
+	log.Print("Binaries downloaded successfully.")
+
+	log.Print("Extracting files...")
+	err = unzipArchive("temp/arbelict.zip", "temp")
+	if err != nil {
+		log.Fatal("Installation failed: ", err)
+	}
+	log.Print("Files extracted successfully.")
 }
 
 func userChoice() (version string, packageType string, err error) {
@@ -93,5 +102,69 @@ func downloadBinaries(packageRoot string, version string) error {
 	if err != nil {
 		return fmt.Errorf("failed to save file: %w", err)
 	}
+	return nil
+}
+
+func unzipArchive(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip (Directory traversal)
+		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", path)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
